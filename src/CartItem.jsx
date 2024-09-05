33930cce90a41c8e9461ac1794ed5aea6f287fdd
sgerster <s.m.gerster@gmail.com>
2024-09-05 09:38:45 -0400

    update remove item

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -29,7 +29,7 @@ const CartItem = ({ onContinueShopping }) => {
    if (item.quantity > 1) {
     dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
    } else {
-    dispatch(removeItem(item));
+    dispatch(removeItem(item)); 
    }
   };
 
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -15,8 +15,8 @@ export const CartSlice = createSlice({
             state.items.push({ name, image, cost, quantity: 1 });
         }
     },
-    removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+    removeItem: (state) => {
+        state.items = state.items.splice(index, 1);
     },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload;
